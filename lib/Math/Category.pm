f776565916e55ab4d4ffab6c771a45c0bb6597dc
hiratara <hiratara@cpan.org>
2009-08-17 17:35:11 +0900

    initial commit

---
+++ lib/Math/Category.pm
@@ -0,0 +1,33 @@
+package Math::Category;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Math::Category -
+
+=head1 SYNOPSIS
+
+  use Math::Category;
+
+=head1 DESCRIPTION
+
+Math::Category is
+
+=head1 AUTHOR
+
+hiratara E<lt>hira.tara@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
