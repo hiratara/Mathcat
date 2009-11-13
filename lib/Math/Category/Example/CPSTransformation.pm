d39afecbf87ff8481af099ebbcd78529aa1f37a7
hiratara <hiratara@cpan.org>
2009-11-13 23:58:39 +0900

    CPS transformation with Yoneda embeddid functor.

--- lib/Math/Category/Example/CPSTransformation.pm
+++ lib/Math/Category/Example/CPSTransformation.pm
@@ -4,7 +4,7 @@ use warnings;
 use Math::Category::Impl::OppositeMorphism;
 use Math::Category::Impl::Bimorphism;
 use Math::Category::Impl::SubroutineMorphism;
-use Math::Category::Impl::Functors qw($HOM_BIFUNCTOR);
+use Math::Category::Impl::Functors qw($YONEDA_EMBEDDING);
 
 use Sub::Exporter -setup => {
     exports => [ qw(cps_transformation) ],
@@ -14,23 +14,33 @@ use Sub::Exporter -setup => {
 sub cps_transformation {
     my $f = shift;
 
-    my $morph = Math::Category::Impl::SubroutineMorphism->new_with_sub( $f );
-    my $hom_morph = $HOM_BIFUNCTOR->(
-        Math::Category::Impl::Bimorphism->new(
-            morphism1 => Math::Category::Impl::OppositeMorphism->new(
-                morphism => $morph,
-            ),
-            morphism2 => $Math::Category::Impl::SubroutineMorphism::ID,
-        )
+    # Wrap as morphism of Sets^op
+    my $morph = Math::Category::Impl::OppositeMorphism->new(
+        morphism => Math::Category::Impl::SubroutineMorphism->new_with_sub( 
+            $f 
+        ),
     );
+
+    # Get natural transformation corresponded to $f.
+    my $fun_morph = $YONEDA_EMBEDDING->( $morph );
+    my $nat = $fun_morph->natural_transformation;
 
     return sub {
         my ($cont, @params) = @_;
-        my $source_hom = 
-               Math::Category::Impl::SubroutineMorphism->new_with_sub( $cont );
-        my $target_hom = $hom_morph->subroutine->( $source_hom );
 
-        return $target_hom->subroutine->( @params );
+        # Wrap as morphism of Sets
+        my $cont_morph = Math::Category::Impl::SubroutineMorphism
+                         ->new_with_sub( $cont );
+
+        # Get component of target of $cont.
+        my $component = $nat->( $cont_morph->target );
+        my $sub       = $component->subroutine;
+
+        # Get CPS tarnsformed subroutine
+        my $cps_transformed = $sub->( $cont_morph )->subroutine;
+
+        # Apply arguments
+        return $cps_transformed->(@params);
     };
 }
 
