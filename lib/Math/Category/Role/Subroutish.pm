00ce2bf74d907ba492536c41db10661e95b38b27
hiratara <hiratara@cpan.org>
2009-11-18 21:01:37 +0900

    Now Subroutish uses Util::Subroutine instead of a raw perl sub {}.

--- lib/Math/Category/Role/Subroutish.pm
+++ lib/Math/Category/Role/Subroutish.pm
@@ -1,13 +1,18 @@
 package Math::Category::Role::Subroutish;
 use Moose::Role;
+use Math::Category::Util::Subroutine;
 
-has impl => ( is => 'ro', isa => 'CodeRef', required => 1 );
+has impl => (
+	is       => 'ro',
+	isa      => 'Math::Category::Util::Subroutine',
+	required => 1
+);
 
 around BUILDARGS => sub {
 	my $super = shift;
 	my $class = shift;
 	my ( $impl ) = @_;
-	return $class->$super( impl => $impl );
+	return $class->$super( impl => wrap $impl );
 };
 
 no  Moose::Role;
